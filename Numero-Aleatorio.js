6c027253c75b3395b90cb37b587c0b52d09d5c1f
Miguel Altamirano Moreno <90479868+CoMike01@users.noreply.github.com>
2023-03-03 07:49:59 -0600

    Update Numero-Aleatorio.js

--- Numero-Aleatorio.js
+++ Numero-Aleatorio.js
@@ -1,10 +1,33 @@
-// Definir una función que genera un número aleatorio entre dos valores
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.get('/random',(_,res) => {
+  res.send({
+    // Definir una función que genera un número aleatorio entre dos valores
 function generarNumeroAleatorio(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // Llamar a la función para generar un número aleatorio entre 1 y 10
 var numeroAleatorio = generarNumeroAleatorio(1, 10);
+  });
+});
 
-// Mostrar el número aleatorio en la consola
-console.log("El número aleatorio es: " + numeroAleatorio);
+app.post('/', (_, res) => {
+  res.send({
+    message: 'Foo bar'
+  });
+});
+
+app.put('/foo', (req, res) => {
+  res.send({
+    method: req.method,
+    url: req.url
+  })
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT :${PORT}`);
+});
